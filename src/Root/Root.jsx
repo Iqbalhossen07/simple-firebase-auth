ce37875d7007560bf321adac2b0961e7ececc9c8
iqbal07 <iqbalhossen0711@gmail.com>
2026-03-14 10:39:01 +0600

    footer has been added

--- src/Root/Root.jsx
+++ src/Root/Root.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from '../components/Header/Header'
 import { Outlet } from 'react-router'
+import Footer from '../components/Footer/Footer'
 
 function Root() {
   return (
-      <div>
+      <div className='w-11/12 mx-auto'>
           <Header></Header>
           <Outlet></Outlet>
+          <Footer></Footer>
     </div>
   )
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
+import { NavLink } from 'react-router';
 
 function Header() {
+   
   return (
     <div className="navbar bg-base-100 shadow-sm">
       <div className="navbar-start">
@@ -27,21 +29,11 @@ function Header() {
             className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
           >
             <li>
-              <a>Item 1</a>
+              <NavLink to={"/"}> Home</NavLink>
             </li>
+
             <li>
-              <a>Parent</a>
-              <ul className="p-2">
-                <li>
-                  <a>Submenu 1</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <a>Item 3</a>
+              <NavLink to={"/about"}> About</NavLink>
             </li>
           </ul>
         </div>
@@ -50,23 +42,10 @@ function Header() {
       <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal px-1">
           <li>
-            <a>Item 1</a>
+            <NavLink to={"/"}>Home</NavLink>
           </li>
           <li>
-            <details>
-              <summary>Parent</summary>
-              <ul className="p-2 bg-base-100 w-40 z-1">
-                <li>
-                  <a>Submenu 1</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
-            </details>
-          </li>
-          <li>
-            <a>Item 3</a>
+            <NavLink to={"/about"}>About</NavLink>
           </li>
         </ul>
       </div>
