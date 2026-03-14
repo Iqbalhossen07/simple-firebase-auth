e1f582637a45d63e29643310e14342e4535433a5
iqbal07 <iqbalhossen0711@gmail.com>
2026-03-14 10:31:54 +0600

    react router has been added

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,26 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+import Root from './Root/Root.jsx';
+import Home from './Pages/Home/Home.jsx';
+import About from './Pages/About/About.jsx';
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: Root,
+    children: [
+      { index: true, Component: Home },
+      { path: "about", Component: About },
+     
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
-)
+);
